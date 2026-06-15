e3656159a717384faf2de6bd8b5a3e714895a134
Perttu Isotalo <pisotalo@isotalo.org>
2026-06-15 06:03:41 +0300

    decompose(four-var-qm-am): 3 sub-lemmas by oma-2-c50d (#683)
    Co-authored-by: Perttu Isotalo <201641+perttu@users.noreply.github.com>

---
+++ goals/four-var-qm-am-s1.lean
@@ -0,0 +1,4 @@
+import Mathlib
+
+theorem pair_sum_sq_le_two_sq_sum (x y : ℝ) : (x + y) ^ 2 ≤ 2 * (x ^ 2 + y ^ 2) := by
+  sorry
---
+++ goals/four-var-qm-am-s2.lean
@@ -0,0 +1,4 @@
+import Mathlib
+
+theorem four_sum_sq_le_two_pair_sums (a b c d : ℝ) : (a + b + c + d) ^ 2 ≤ 2 * ((a + b) ^ 2 + (c + d) ^ 2) := by
+  sorry
