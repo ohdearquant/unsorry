40042c03a3e8880d1e61bf8cb241d0968d3d4d6f
Perttu Isotalo <pisotalo@isotalo.org>
2026-06-16 22:25:27 +0300

    decompose(six-dvd-pow-three-add-five-mul): 2 sub-lemmas by oma-2-c50d (#1182)
    Co-authored-by: perttu-mp <289810485+perttu-mp@users.noreply.github.com>

---
+++ goals/six-dvd-pow-three-add-five-mul-s1.lean
@@ -0,0 +1,4 @@
+import Mathlib
+
+theorem two_dvd_pow_three_add_five_mul (n : ℤ) : (2 : ℤ) ∣ n ^ 3 + 5 * n := by
+  sorry
---
+++ goals/six-dvd-pow-three-add-five-mul-s2.lean
@@ -0,0 +1,4 @@
+import Mathlib
+
+theorem three_dvd_pow_three_add_five_mul (n : ℤ) : (3 : ℤ) ∣ n ^ 3 + 5 * n := by
+  sorry
