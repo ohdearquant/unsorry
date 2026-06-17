30d8735bb8220cffaf0fb6386d19e8684f89814b
Perttu Isotalo <pisotalo@isotalo.org>
2026-06-17 15:51:56 +0300

    prove(dvd-330-pow-twentythree-sub-pow-three): dvd_330_pow_twentythree_sub_pow_three by mac-158f (#1967)
    Co-authored-by: OceanLi <122793010+ohdearquant@users.noreply.github.com>

---
+++ library/Unsorry/Dvd330PowTwentythreeSubPowThree.lean
@@ -0,0 +1,44 @@
+import Mathlib
+
+set_option maxRecDepth 8000 in
+theorem dvd_330_pow_twentythree_sub_pow_three (n : ℤ) :
+    (330 : ℤ) ∣ n ^ 23 - n ^ 3 := by
+  have h0 : (2 : ℤ) ∣ n ^ 23 - n ^ 3 := by
+    suffices hh : ((n ^ 23 - n ^ 3 : ℤ) : ZMod 2) = 0 by
+      exact_mod_cast (ZMod.intCast_zmod_eq_zero_iff_dvd (n ^ 23 - n ^ 3) 2).mp hh
+    push_cast
+    generalize (n : ZMod 2) = x
+    revert x
+    decide
+  have h1 : (3 : ℤ) ∣ n ^ 23 - n ^ 3 := by
+    suffices hh : ((n ^ 23 - n ^ 3 : ℤ) : ZMod 3) = 0 by
+      exact_mod_cast (ZMod.intCast_zmod_eq_zero_iff_dvd (n ^ 23 - n ^ 3) 3).mp hh
+    push_cast
+    generalize (n : ZMod 3) = x
+    revert x
+    decide
+  have h2 : (5 : ℤ) ∣ n ^ 23 - n ^ 3 := by
+    suffices hh : ((n ^ 23 - n ^ 3 : ℤ) : ZMod 5) = 0 by
+      exact_mod_cast (ZMod.intCast_zmod_eq_zero_iff_dvd (n ^ 23 - n ^ 3) 5).mp hh
+    push_cast
+    generalize (n : ZMod 5) = x
+    revert x
+    decide
+  have h3 : (11 : ℤ) ∣ n ^ 23 - n ^ 3 := by
+    suffices hh : ((n ^ 23 - n ^ 3 : ℤ) : ZMod 11) = 0 by
+      exact_mod_cast (ZMod.intCast_zmod_eq_zero_iff_dvd (n ^ 23 - n ^ 3) 11).mp hh
+    push_cast
+    generalize (n : ZMod 11) = x
+    revert x
+    decide
+  have co1 : IsCoprime ((2) : ℤ) (3 : ℤ) := by
+    rw [Int.isCoprime_iff_gcd_eq_one]; decide
+  have acc1 : ((2) * 3 : ℤ) ∣ n ^ 23 - n ^ 3 := co1.mul_dvd h0 h1
+  have co2 : IsCoprime (((2) * 3) : ℤ) (5 : ℤ) := by
+    rw [Int.isCoprime_iff_gcd_eq_one]; decide
+  have acc2 : (((2) * 3) * 5 : ℤ) ∣ n ^ 23 - n ^ 3 := co2.mul_dvd acc1 h2
+  have co3 : IsCoprime ((((2) * 3) * 5) : ℤ) (11 : ℤ) := by
+    rw [Int.isCoprime_iff_gcd_eq_one]; decide
+  have acc3 : ((((2) * 3) * 5) * 11 : ℤ) ∣ n ^ 23 - n ^ 3 := co3.mul_dvd acc2 h3
+  have keq : (((((2) * 3) * 5) * 11) : ℤ) = (330 : ℤ) := by norm_num
+  rwa [keq] at acc3
