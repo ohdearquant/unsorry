7f64ceee6fc643f7189a06ae65e0a19b36588373
OceanLi <quantocean.li@gmail.com>
2026-06-16 20:39:19 -0400

    prove(dvd-1365-pow-thirteen-sub-self): dvd_1365_pow_thirteen_sub_self by mac-158f (#1234)

---
+++ library/Unsorry/Dvd1365PowThirteenSubSelf.lean
@@ -0,0 +1,43 @@
+import Mathlib
+
+set_option maxRecDepth 20000 in
+theorem dvd_1365_pow_thirteen_sub_self (n : ℤ) : (1365 : ℤ) ∣ n ^ 13 - n := by
+  have h0 : (3 : ℤ) ∣ n ^ 13 - n := by
+    suffices hh : ((n ^ 13 - n : ℤ) : ZMod 3) = 0 by
+      exact_mod_cast (ZMod.intCast_zmod_eq_zero_iff_dvd (n ^ 13 - n) 3).mp hh
+    push_cast
+    generalize (n : ZMod 3) = x
+    revert x
+    decide
+  have h1 : (5 : ℤ) ∣ n ^ 13 - n := by
+    suffices hh : ((n ^ 13 - n : ℤ) : ZMod 5) = 0 by
+      exact_mod_cast (ZMod.intCast_zmod_eq_zero_iff_dvd (n ^ 13 - n) 5).mp hh
+    push_cast
+    generalize (n : ZMod 5) = x
+    revert x
+    decide
+  have h2 : (7 : ℤ) ∣ n ^ 13 - n := by
+    suffices hh : ((n ^ 13 - n : ℤ) : ZMod 7) = 0 by
+      exact_mod_cast (ZMod.intCast_zmod_eq_zero_iff_dvd (n ^ 13 - n) 7).mp hh
+    push_cast
+    generalize (n : ZMod 7) = x
+    revert x
+    decide
+  have h3 : (13 : ℤ) ∣ n ^ 13 - n := by
+    suffices hh : ((n ^ 13 - n : ℤ) : ZMod 13) = 0 by
+      exact_mod_cast (ZMod.intCast_zmod_eq_zero_iff_dvd (n ^ 13 - n) 13).mp hh
+    push_cast
+    generalize (n : ZMod 13) = x
+    revert x
+    decide
+  have co1 : IsCoprime ((3) : ℤ) (5 : ℤ) := by
+    rw [Int.isCoprime_iff_gcd_eq_one]; decide
+  have acc1 : ((3) * 5 : ℤ) ∣ n ^ 13 - n := co1.mul_dvd h0 h1
+  have co2 : IsCoprime (((3) * 5) : ℤ) (7 : ℤ) := by
+    rw [Int.isCoprime_iff_gcd_eq_one]; decide
+  have acc2 : (((3) * 5) * 7 : ℤ) ∣ n ^ 13 - n := co2.mul_dvd acc1 h2
+  have co3 : IsCoprime ((((3) * 5) * 7) : ℤ) (13 : ℤ) := by
+    rw [Int.isCoprime_iff_gcd_eq_one]; decide
+  have acc3 : ((((3) * 5) * 7) * 13 : ℤ) ∣ n ^ 13 - n := co3.mul_dvd acc2 h3
+  have keq : (((((3) * 5) * 7) * 13) : ℤ) = (1365 : ℤ) := by norm_num
+  rwa [keq] at acc3
