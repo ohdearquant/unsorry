b20722830438d08d347e6167c038953f9fea8ce6
OceanLi <quantocean.li@gmail.com>
2026-06-18 03:19:35 -0400

    prove(two-sum-cubes-ge-sym-quadratics): two_sum_cubes_ge_sym_quadratics by mac-158f (#2091)

---
+++ library/Unsorry/TwoSumCubesGeSymQuadratics.lean
@@ -0,0 +1,11 @@
+import Mathlib
+
+set_option linter.unusedTactic false in
+set_option linter.unreachableTactic false in
+theorem two_sum_cubes_ge_sym_quadratics (a b c : ℝ) (ha : 0 ≤ a) (hb : 0 ≤ b) (hc : 0 ≤ c) : a * b * (a + b) + b * c * (b + c) + c * a * (c + a) ≤ 2 * (a ^ 3 + b ^ 3 + c ^ 3) := by
+  first
+    | nlinarith [sq_nonneg a, sq_nonneg b, sq_nonneg c, sq_nonneg (a - b), sq_nonneg (a + b), sq_nonneg (a^2 - b^2), sq_nonneg (a*b), sq_nonneg (a - c), sq_nonneg (a + c), sq_nonneg (a^2 - c^2), sq_nonneg (a*c), sq_nonneg (b - c), sq_nonneg (b + c), sq_nonneg (b^2 - c^2), sq_nonneg (b*c), sq_nonneg (a + b - c), mul_nonneg ha hb, mul_nonneg ha hc, mul_nonneg hb hc, mul_nonneg ha (sq_nonneg (b - c)), mul_nonneg hb (sq_nonneg (a - c)), mul_nonneg hc (sq_nonneg (a - b)), mul_nonneg (mul_nonneg ha hb) hc]
+    | positivity
+    | omega
+    | (nlinarith [sq_nonneg a, sq_nonneg b, sq_nonneg c, sq_nonneg (a - b), sq_nonneg (a + b), sq_nonneg (a^2 - b^2), sq_nonneg (a*b), sq_nonneg (a - c), sq_nonneg (a + c), sq_nonneg (a^2 - c^2), sq_nonneg (a*c), sq_nonneg (b - c), sq_nonneg (b + c), sq_nonneg (b^2 - c^2), sq_nonneg (b*c), sq_nonneg (a + b - c), mul_nonneg ha hb, mul_nonneg ha hc, mul_nonneg hb hc, mul_nonneg ha (sq_nonneg (b - c)), mul_nonneg hb (sq_nonneg (a - c)), mul_nonneg hc (sq_nonneg (a - b)), mul_nonneg (mul_nonneg ha hb) hc, sq_nonneg (1:ℝ)])
+    | (field_simp; rw [div_le_div_iff] <;> nlinarith [sq_nonneg a, sq_nonneg b, sq_nonneg c, sq_nonneg (a - b), sq_nonneg (a + b), sq_nonneg (a^2 - b^2), sq_nonneg (a*b), sq_nonneg (a - c), sq_nonneg (a + c), sq_nonneg (a^2 - c^2), sq_nonneg (a*c), sq_nonneg (b - c), sq_nonneg (b + c), sq_nonneg (b^2 - c^2), sq_nonneg (b*c), sq_nonneg (a + b - c), mul_nonneg ha hb, mul_nonneg ha hc, mul_nonneg hb hc, mul_nonneg ha (sq_nonneg (b - c)), mul_nonneg hb (sq_nonneg (a - c)), mul_nonneg hc (sq_nonneg (a - b)), mul_nonneg (mul_nonneg ha hb) hc])
