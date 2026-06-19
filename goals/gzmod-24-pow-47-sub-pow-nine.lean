c4a4ae39660ae8b37187c55add344e2c0165ef56
OceanLi <quantocean.li@gmail.com>
2026-06-19 08:43:12 -0400

    prove(gzmod-24-pow-47-sub-pow-nine): gzmod_24_pow_47_sub_pow_nine by mac-158f (#2513)

---
+++ goals/gzmod-24-pow-47-sub-pow-nine.lean
@@ -0,0 +1,4 @@
+import Mathlib
+
+theorem gzmod_24_pow_47_sub_pow_nine (n : ℤ) : (24 : ℤ) ∣ n ^ 47 - n ^ 9 := by
+  sorry
---
+++ library/Unsorry/Gzmod24Pow47SubPowNine.lean
@@ -0,0 +1,8 @@
+import Mathlib
+
+set_option maxRecDepth 40000 in
+theorem gzmod_24_pow_47_sub_pow_nine (n : ℤ) : (24 : ℤ) ∣ n ^ 47 - n ^ 9 := by
+  have h : ∀ m : ZMod 24, m ^ 47 - m ^ 9 = 0 := by decide
+  have hz : ((n ^ 47 - n ^ 9 : ℤ) : ZMod 24) = 0 := by push_cast; exact h _
+  have hdvd := (ZMod.intCast_zmod_eq_zero_iff_dvd (n ^ 47 - n ^ 9) 24).mp hz
+  exact_mod_cast hdvd
