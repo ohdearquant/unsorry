48841c240cdc7b180d1e6505be6b4903b2dba6da
OceanLi <quantocean.li@gmail.com>
2026-06-19 07:26:06 -0400

    prove(gzmod-24-pow-38-sub-pow-twelve): gzmod_24_pow_38_sub_pow_twelve by mac-158f (#2469)

---
+++ goals/gzmod-24-pow-38-sub-pow-twelve.lean
@@ -0,0 +1,4 @@
+import Mathlib
+
+theorem gzmod_24_pow_38_sub_pow_twelve (n : ℤ) : (24 : ℤ) ∣ n ^ 38 - n ^ 12 := by
+  sorry
---
+++ library/Unsorry/Gzmod24Pow38SubPowTwelve.lean
@@ -0,0 +1,8 @@
+import Mathlib
+
+set_option maxRecDepth 40000 in
+theorem gzmod_24_pow_38_sub_pow_twelve (n : ℤ) : (24 : ℤ) ∣ n ^ 38 - n ^ 12 := by
+  have h : ∀ m : ZMod 24, m ^ 38 - m ^ 12 = 0 := by decide
+  have hz : ((n ^ 38 - n ^ 12 : ℤ) : ZMod 24) = 0 := by push_cast; exact h _
+  have hdvd := (ZMod.intCast_zmod_eq_zero_iff_dvd (n ^ 38 - n ^ 12) 24).mp hz
+  exact_mod_cast hdvd
