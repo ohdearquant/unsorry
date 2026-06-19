abd2ddf8a551f20bb0c293fc9d7525bfdb8bc4f1
OceanLi <quantocean.li@gmail.com>
2026-06-19 06:56:59 -0400

    prove(gzmod-24-pow-29-sub-pow-three): gzmod_24_pow_29_sub_pow_three by mac-158f (#2440)

---
+++ goals/gzmod-24-pow-29-sub-pow-three.lean
@@ -0,0 +1,4 @@
+import Mathlib
+
+theorem gzmod_24_pow_29_sub_pow_three (n : ℤ) : (24 : ℤ) ∣ n ^ 29 - n ^ 3 := by
+  sorry
---
+++ library/Unsorry/Gzmod24Pow29SubPowThree.lean
@@ -0,0 +1,8 @@
+import Mathlib
+
+set_option maxRecDepth 40000 in
+theorem gzmod_24_pow_29_sub_pow_three (n : ℤ) : (24 : ℤ) ∣ n ^ 29 - n ^ 3 := by
+  have h : ∀ m : ZMod 24, m ^ 29 - m ^ 3 = 0 := by decide
+  have hz : ((n ^ 29 - n ^ 3 : ℤ) : ZMod 24) = 0 := by push_cast; exact h _
+  have hdvd := (ZMod.intCast_zmod_eq_zero_iff_dvd (n ^ 29 - n ^ 3) 24).mp hz
+  exact_mod_cast hdvd
