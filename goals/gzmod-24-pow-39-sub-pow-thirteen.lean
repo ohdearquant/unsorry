a47776b83b8ef3a4123e749d4f87b87b489705c7
OceanLi <quantocean.li@gmail.com>
2026-06-19 07:38:54 -0400

    prove(gzmod-24-pow-39-sub-pow-thirteen): gzmod_24_pow_39_sub_pow_thirteen by mac-158f (#2474)

---
+++ goals/gzmod-24-pow-39-sub-pow-thirteen.lean
@@ -0,0 +1,4 @@
+import Mathlib
+
+theorem gzmod_24_pow_39_sub_pow_thirteen (n : ℤ) : (24 : ℤ) ∣ n ^ 39 - n ^ 13 := by
+  sorry
---
+++ library/Unsorry/Gzmod24Pow39SubPowThirteen.lean
@@ -0,0 +1,8 @@
+import Mathlib
+
+set_option maxRecDepth 40000 in
+theorem gzmod_24_pow_39_sub_pow_thirteen (n : ℤ) : (24 : ℤ) ∣ n ^ 39 - n ^ 13 := by
+  have h : ∀ m : ZMod 24, m ^ 39 - m ^ 13 = 0 := by decide
+  have hz : ((n ^ 39 - n ^ 13 : ℤ) : ZMod 24) = 0 := by push_cast; exact h _
+  have hdvd := (ZMod.intCast_zmod_eq_zero_iff_dvd (n ^ 39 - n ^ 13) 24).mp hz
+  exact_mod_cast hdvd
