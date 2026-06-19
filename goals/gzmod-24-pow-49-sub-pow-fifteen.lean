6c7efda2f58934b08e4e163e33a64f55c74e0139
OceanLi <quantocean.li@gmail.com>
2026-06-19 08:40:43 -0400

    prove(gzmod-24-pow-49-sub-pow-fifteen): gzmod_24_pow_49_sub_pow_fifteen by mac-158f (#2524)

---
+++ goals/gzmod-24-pow-49-sub-pow-fifteen.lean
@@ -0,0 +1,4 @@
+import Mathlib
+
+theorem gzmod_24_pow_49_sub_pow_fifteen (n : ℤ) : (24 : ℤ) ∣ n ^ 49 - n ^ 15 := by
+  sorry
---
+++ library/Unsorry/Gzmod24Pow49SubPowFifteen.lean
@@ -0,0 +1,8 @@
+import Mathlib
+
+set_option maxRecDepth 40000 in
+theorem gzmod_24_pow_49_sub_pow_fifteen (n : ℤ) : (24 : ℤ) ∣ n ^ 49 - n ^ 15 := by
+  have h : ∀ m : ZMod 24, m ^ 49 - m ^ 15 = 0 := by decide
+  have hz : ((n ^ 49 - n ^ 15 : ℤ) : ZMod 24) = 0 := by push_cast; exact h _
+  have hdvd := (ZMod.intCast_zmod_eq_zero_iff_dvd (n ^ 49 - n ^ 15) 24).mp hz
+  exact_mod_cast hdvd
