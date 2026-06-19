77df5a232836b378c8b57c295c608081c0bf60dc
OceanLi <quantocean.li@gmail.com>
2026-06-18 23:10:34 -0400

    prove(gzmod-240-pow-twelve-sub-pow-four): gzmod_240_pow_twelve_sub_pow_four by mac-158f (#2287)

---
+++ library/Unsorry/Gzmod240PowTwelveSubPowFour.lean
@@ -0,0 +1,8 @@
+import Mathlib
+
+set_option maxRecDepth 40000 in
+theorem gzmod_240_pow_twelve_sub_pow_four (n : ℤ) : (240 : ℤ) ∣ n ^ 12 - n ^ 4 := by
+  have h : ∀ m : ZMod 240, m ^ 12 - m ^ 4 = 0 := by decide
+  have hz : ((n ^ 12 - n ^ 4 : ℤ) : ZMod 240) = 0 := by push_cast; exact h _
+  have hdvd := (ZMod.intCast_zmod_eq_zero_iff_dvd (n ^ 12 - n ^ 4) 240).mp hz
+  exact_mod_cast hdvd
