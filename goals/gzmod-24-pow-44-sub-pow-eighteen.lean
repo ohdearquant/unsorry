fd620680bb6d34925a12aadd2dc762019fa248bc
OceanLi <quantocean.li@gmail.com>
2026-06-19 08:18:34 -0400

    prove(gzmod-24-pow-44-sub-pow-eighteen): gzmod_24_pow_44_sub_pow_eighteen by mac-158f (#2497)

---
+++ goals/gzmod-24-pow-44-sub-pow-eighteen.lean
@@ -0,0 +1,4 @@
+import Mathlib
+
+theorem gzmod_24_pow_44_sub_pow_eighteen (n : ℤ) : (24 : ℤ) ∣ n ^ 44 - n ^ 18 := by
+  sorry
---
+++ library/Unsorry/Gzmod24Pow44SubPowEighteen.lean
@@ -0,0 +1,8 @@
+import Mathlib
+
+set_option maxRecDepth 40000 in
+theorem gzmod_24_pow_44_sub_pow_eighteen (n : ℤ) : (24 : ℤ) ∣ n ^ 44 - n ^ 18 := by
+  have h : ∀ m : ZMod 24, m ^ 44 - m ^ 18 = 0 := by decide
+  have hz : ((n ^ 44 - n ^ 18 : ℤ) : ZMod 24) = 0 := by push_cast; exact h _
+  have hdvd := (ZMod.intCast_zmod_eq_zero_iff_dvd (n ^ 44 - n ^ 18) 24).mp hz
+  exact_mod_cast hdvd
