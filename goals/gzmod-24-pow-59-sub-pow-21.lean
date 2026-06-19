0ab8521bd7c69b77a3732249e23a3c9e3782654d
OceanLi <quantocean.li@gmail.com>
2026-06-19 09:37:37 -0400

    prove(gzmod-24-pow-59-sub-pow-21): gzmod_24_pow_59_sub_pow_21 by mac-158f (#2569)

---
+++ goals/gzmod-24-pow-59-sub-pow-21.lean
@@ -0,0 +1,4 @@
+import Mathlib
+
+theorem gzmod_24_pow_59_sub_pow_21 (n : ℤ) : (24 : ℤ) ∣ n ^ 59 - n ^ 21 := by
+  sorry
---
+++ library/Unsorry/Gzmod24Pow59SubPow21.lean
@@ -0,0 +1,8 @@
+import Mathlib
+
+set_option maxRecDepth 40000 in
+theorem gzmod_24_pow_59_sub_pow_21 (n : ℤ) : (24 : ℤ) ∣ n ^ 59 - n ^ 21 := by
+  have h : ∀ m : ZMod 24, m ^ 59 - m ^ 21 = 0 := by decide
+  have hz : ((n ^ 59 - n ^ 21 : ℤ) : ZMod 24) = 0 := by push_cast; exact h _
+  have hdvd := (ZMod.intCast_zmod_eq_zero_iff_dvd (n ^ 59 - n ^ 21) 24).mp hz
+  exact_mod_cast hdvd
