2f68bec4e6a3824d327d7b28d6fa037300e0c299
OceanLi <quantocean.li@gmail.com>
2026-06-19 09:11:48 -0400

    prove(gzmod-24-pow-55-sub-pow-29): gzmod_24_pow_55_sub_pow_29 by mac-158f (#2552)

---
+++ goals/gzmod-24-pow-55-sub-pow-29.lean
@@ -0,0 +1,4 @@
+import Mathlib
+
+theorem gzmod_24_pow_55_sub_pow_29 (n : ℤ) : (24 : ℤ) ∣ n ^ 55 - n ^ 29 := by
+  sorry
---
+++ library/Unsorry/Gzmod24Pow55SubPow29.lean
@@ -0,0 +1,8 @@
+import Mathlib
+
+set_option maxRecDepth 40000 in
+theorem gzmod_24_pow_55_sub_pow_29 (n : ℤ) : (24 : ℤ) ∣ n ^ 55 - n ^ 29 := by
+  have h : ∀ m : ZMod 24, m ^ 55 - m ^ 29 = 0 := by decide
+  have hz : ((n ^ 55 - n ^ 29 : ℤ) : ZMod 24) = 0 := by push_cast; exact h _
+  have hdvd := (ZMod.intCast_zmod_eq_zero_iff_dvd (n ^ 55 - n ^ 29) 24).mp hz
+  exact_mod_cast hdvd
