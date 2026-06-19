87a33c05c315ec49831dbc88a14e5edb1a090d31
OceanLi <quantocean.li@gmail.com>
2026-06-19 07:44:22 -0400

    prove(gzmod-24-pow-40-sub-pow-fourteen): gzmod_24_pow_40_sub_pow_fourteen by mac-158f (#2477)

---
+++ goals/gzmod-24-pow-40-sub-pow-fourteen.lean
@@ -0,0 +1,4 @@
+import Mathlib
+
+theorem gzmod_24_pow_40_sub_pow_fourteen (n : ℤ) : (24 : ℤ) ∣ n ^ 40 - n ^ 14 := by
+  sorry
---
+++ library/Unsorry/Gzmod24Pow40SubPowFourteen.lean
@@ -0,0 +1,8 @@
+import Mathlib
+
+set_option maxRecDepth 40000 in
+theorem gzmod_24_pow_40_sub_pow_fourteen (n : ℤ) : (24 : ℤ) ∣ n ^ 40 - n ^ 14 := by
+  have h : ∀ m : ZMod 24, m ^ 40 - m ^ 14 = 0 := by decide
+  have hz : ((n ^ 40 - n ^ 14 : ℤ) : ZMod 24) = 0 := by push_cast; exact h _
+  have hdvd := (ZMod.intCast_zmod_eq_zero_iff_dvd (n ^ 40 - n ^ 14) 24).mp hz
+  exact_mod_cast hdvd
