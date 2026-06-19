3c73eaf7c8450b268c988f26d8dbe97d756ed94b
OceanLi <quantocean.li@gmail.com>
2026-06-19 07:22:27 -0400

    prove(gzmod-24-pow-32-sub-pow-six): gzmod_24_pow_32_sub_pow_six by mac-158f (#2449)

---
+++ goals/gzmod-24-pow-32-sub-pow-six.lean
@@ -0,0 +1,4 @@
+import Mathlib
+
+theorem gzmod_24_pow_32_sub_pow_six (n : ℤ) : (24 : ℤ) ∣ n ^ 32 - n ^ 6 := by
+  sorry
---
+++ library/Unsorry/Gzmod24Pow32SubPowSix.lean
@@ -0,0 +1,8 @@
+import Mathlib
+
+set_option maxRecDepth 40000 in
+theorem gzmod_24_pow_32_sub_pow_six (n : ℤ) : (24 : ℤ) ∣ n ^ 32 - n ^ 6 := by
+  have h : ∀ m : ZMod 24, m ^ 32 - m ^ 6 = 0 := by decide
+  have hz : ((n ^ 32 - n ^ 6 : ℤ) : ZMod 24) = 0 := by push_cast; exact h _
+  have hdvd := (ZMod.intCast_zmod_eq_zero_iff_dvd (n ^ 32 - n ^ 6) 24).mp hz
+  exact_mod_cast hdvd
