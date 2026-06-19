cc1712f94c9dc0f49a14ed6591b4a3f980652806
OceanLi <quantocean.li@gmail.com>
2026-06-19 08:44:33 -0400

    prove(gzmod-24-pow-50-sub-pow-sixteen): gzmod_24_pow_50_sub_pow_sixteen by mac-158f (#2528)

---
+++ goals/gzmod-24-pow-50-sub-pow-sixteen.lean
@@ -0,0 +1,4 @@
+import Mathlib
+
+theorem gzmod_24_pow_50_sub_pow_sixteen (n : ℤ) : (24 : ℤ) ∣ n ^ 50 - n ^ 16 := by
+  sorry
---
+++ library/Unsorry/Gzmod24Pow50SubPowSixteen.lean
@@ -0,0 +1,8 @@
+import Mathlib
+
+set_option maxRecDepth 40000 in
+theorem gzmod_24_pow_50_sub_pow_sixteen (n : ℤ) : (24 : ℤ) ∣ n ^ 50 - n ^ 16 := by
+  have h : ∀ m : ZMod 24, m ^ 50 - m ^ 16 = 0 := by decide
+  have hz : ((n ^ 50 - n ^ 16 : ℤ) : ZMod 24) = 0 := by push_cast; exact h _
+  have hdvd := (ZMod.intCast_zmod_eq_zero_iff_dvd (n ^ 50 - n ^ 16) 24).mp hz
+  exact_mod_cast hdvd
