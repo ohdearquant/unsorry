467ffa7a19ff9def69f65725bd7587f2b7a1682e
OceanLi <quantocean.li@gmail.com>
2026-06-19 08:13:23 -0400

    prove(gzmod-24-pow-45-sub-pow-seven): gzmod_24_pow_45_sub_pow_seven by mac-158f (#2504)

---
+++ goals/gzmod-24-pow-45-sub-pow-seven.lean
@@ -0,0 +1,4 @@
+import Mathlib
+
+theorem gzmod_24_pow_45_sub_pow_seven (n : ℤ) : (24 : ℤ) ∣ n ^ 45 - n ^ 7 := by
+  sorry
---
+++ library/Unsorry/Gzmod24Pow45SubPowSeven.lean
@@ -0,0 +1,8 @@
+import Mathlib
+
+set_option maxRecDepth 40000 in
+theorem gzmod_24_pow_45_sub_pow_seven (n : ℤ) : (24 : ℤ) ∣ n ^ 45 - n ^ 7 := by
+  have h : ∀ m : ZMod 24, m ^ 45 - m ^ 7 = 0 := by decide
+  have hz : ((n ^ 45 - n ^ 7 : ℤ) : ZMod 24) = 0 := by push_cast; exact h _
+  have hdvd := (ZMod.intCast_zmod_eq_zero_iff_dvd (n ^ 45 - n ^ 7) 24).mp hz
+  exact_mod_cast hdvd
