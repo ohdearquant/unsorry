41bd09c4222077c6ff5d2faa0fe8067f2bdc97fb
OceanLi <quantocean.li@gmail.com>
2026-06-19 04:32:53 -0400

    prove(gzmod-24-pow-22-sub-pow-twenty): gzmod_24_pow_22_sub_pow_twenty by mac-158f (#2371)

---
+++ goals/gzmod-24-pow-22-sub-pow-twenty.lean
@@ -0,0 +1,4 @@
+import Mathlib
+
+theorem gzmod_24_pow_22_sub_pow_twenty (n : ℤ) : (24 : ℤ) ∣ n ^ 22 - n ^ 20 := by
+  sorry
---
+++ library/Unsorry/Gzmod24Pow22SubPowTwenty.lean
@@ -0,0 +1,8 @@
+import Mathlib
+
+set_option maxRecDepth 40000 in
+theorem gzmod_24_pow_22_sub_pow_twenty (n : ℤ) : (24 : ℤ) ∣ n ^ 22 - n ^ 20 := by
+  have h : ∀ m : ZMod 24, m ^ 22 - m ^ 20 = 0 := by decide
+  have hz : ((n ^ 22 - n ^ 20 : ℤ) : ZMod 24) = 0 := by push_cast; exact h _
+  have hdvd := (ZMod.intCast_zmod_eq_zero_iff_dvd (n ^ 22 - n ^ 20) 24).mp hz
+  exact_mod_cast hdvd
