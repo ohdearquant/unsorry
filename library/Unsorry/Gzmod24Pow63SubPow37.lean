67391b1fd02f091f03871e20ff377266ddf4da3e
OceanLi <quantocean.li@gmail.com>
2026-06-19 10:26:59 -0400

    prove(gzmod-24-pow-63-sub-pow-37): gzmod_24_pow_63_sub_pow_37 by mac-158f (#2591)

---
+++ library/Unsorry/Gzmod24Pow63SubPow37.lean
@@ -0,0 +1,8 @@
+import Mathlib
+
+set_option maxRecDepth 40000 in
+theorem gzmod_24_pow_63_sub_pow_37 (n : ℤ) : (24 : ℤ) ∣ n ^ 63 - n ^ 37 := by
+  have h : ∀ m : ZMod 24, m ^ 63 - m ^ 37 = 0 := by decide
+  have hz : ((n ^ 63 - n ^ 37 : ℤ) : ZMod 24) = 0 := by push_cast; exact h _
+  have hdvd := (ZMod.intCast_zmod_eq_zero_iff_dvd (n ^ 63 - n ^ 37) 24).mp hz
+  exact_mod_cast hdvd
