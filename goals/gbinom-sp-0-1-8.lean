a37c3168bee4007aeff735b52f1cc683a7a17371
Perttu Isotalo <pisotalo@isotalo.org>
2026-06-19 21:01:32 +0300

    prove(gbinom-sp-0-1-8): gbinom_sp_0_1_8 by mac-158f (#2716)
    Co-authored-by: OceanLi <122793010+ohdearquant@users.noreply.github.com>

---
+++ goals/gbinom-sp-0-1-8.lean
@@ -0,0 +1,4 @@
+import Mathlib
+
+theorem gbinom_sp_0_1_8 (n : ℤ) : (6 : ℤ) ∣ (n * (n + 1) * (n + 8)) := by
+  sorry
---
+++ library/Unsorry/GbinomSp018.lean
@@ -0,0 +1,8 @@
+import Mathlib
+
+set_option maxRecDepth 40000 in
+theorem gbinom_sp_0_1_8 (n : ℤ) : (6 : ℤ) ∣ (n * (n + 1) * (n + 8)) := by
+  have h : ∀ m : ZMod 6, m * (m + 1) * (m + 8) = 0 := by decide
+  have hz : ((n * (n + 1) * (n + 8) : ℤ) : ZMod 6) = 0 := by push_cast; exact h _
+  have hdvd := (ZMod.intCast_zmod_eq_zero_iff_dvd (n * (n + 1) * (n + 8)) 6).mp hz
+  exact_mod_cast hdvd
