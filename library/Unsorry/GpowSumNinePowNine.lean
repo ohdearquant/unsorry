6319a8fffb50323dbf3a589be0f3a182040f7994
Chris Barlow <cgbarlow@gmail.com>
2026-06-20 18:14:43 +1200

    prove(gpow-sum-nine-pow-nine): gpow_sum_nine_pow_nine by mac-158f (#3063)
    Co-authored-by: OceanLi <122793010+ohdearquant@users.noreply.github.com>

---
+++ library/Unsorry/GpowSumNinePowNine.lean
@@ -0,0 +1,4 @@
+import Mathlib
+
+theorem gpow_sum_nine_pow_nine (n : ℤ) : (n + 9) ∣ (n^9 + 387420489) := by
+  exact ⟨n^8 - 9*n^7 + 81*n^6 - 729*n^5 + 6561*n^4 - 59049*n^3 + 531441*n^2 - 4782969*n + 43046721, by ring⟩
