a53145ae3181d4e8c26c7fa9a1f52788f4ac43ce
Chris Barlow <cgbarlow@gmail.com>
2026-06-20 17:02:09 +1200

    prove(gpow-sum-four-pow-thirteen): gpow_sum_four_pow_thirteen by mac-158f (#2993)
    Co-authored-by: OceanLi <122793010+ohdearquant@users.noreply.github.com>

---
+++ library/Unsorry/GpowSumFourPowThirteen.lean
@@ -0,0 +1,4 @@
+import Mathlib
+
+theorem gpow_sum_four_pow_thirteen (n : ℤ) : (n + 4) ∣ (n^13 + 67108864) := by
+  exact ⟨n^12 - 4*n^11 + 16*n^10 - 64*n^9 + 256*n^8 - 1024*n^7 + 4096*n^6 - 16384*n^5 + 65536*n^4 - 262144*n^3 + 1048576*n^2 - 4194304*n + 16777216, by ring⟩
