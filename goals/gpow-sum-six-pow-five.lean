d02fe16a483952dcafe00d40dfc4585e6a069f26
Chris Barlow <cgbarlow@gmail.com>
2026-06-21 01:52:19 +1200

    prove(gpow-sum-six-pow-five): gpow_sum_six_pow_five by mac-158f (#3282)
    Co-authored-by: OceanLi <122793010+ohdearquant@users.noreply.github.com>

---
+++ goals/gpow-sum-six-pow-five.lean
@@ -0,0 +1,4 @@
+import Mathlib
+
+theorem gpow_sum_six_pow_five (n : ℤ) : (n + 6) ∣ (n^5 + 7776) := by
+  sorry
---
+++ library/Unsorry/GpowSumSixPowFive.lean
@@ -0,0 +1,4 @@
+import Mathlib
+
+theorem gpow_sum_six_pow_five (n : ℤ) : (n + 6) ∣ (n^5 + 7776) := by
+  exact ⟨n^4 - 6*n^3 + 36*n^2 - 216*n + 1296, by ring⟩
