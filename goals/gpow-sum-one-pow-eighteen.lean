89b63b5b29cecdc699d293536b797775dd2be5ca
Perttu Isotalo <pisotalo@isotalo.org>
2026-06-20 09:50:37 +0300

    prove(gpow-sum-one-pow-eighteen): gpow_sum_one_pow_eighteen by mac-158f (#3084)
    Co-authored-by: OceanLi <122793010+ohdearquant@users.noreply.github.com>

---
+++ goals/gpow-sum-one-pow-eighteen.lean
@@ -0,0 +1,4 @@
+import Mathlib
+
+theorem gpow_sum_one_pow_eighteen (n : ℤ) : (n + 1) ∣ (n^18 - 1) := by
+  sorry
---
+++ library/Unsorry/GpowSumOnePowEighteen.lean
@@ -0,0 +1,4 @@
+import Mathlib
+
+theorem gpow_sum_one_pow_eighteen (n : ℤ) : (n + 1) ∣ (n^18 - 1) := by
+  exact ⟨n^17 - n^16 + n^15 - n^14 + n^13 - n^12 + n^11 - n^10 + n^9 - n^8 + n^7 - n^6 + n^5 - n^4 + n^3 - n^2 + n - 1, by ring⟩
