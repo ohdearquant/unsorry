d65e20af22d4f1933daac51c35e21f78db04773e
Chris Barlow <cgbarlow@gmail.com>
2026-06-23 07:35:08 +1200

    prove(gself-pow-two-pow-22-add-pow-seven): gself_pow_two_pow_22_add_pow_seven by mac-158f (#4813)
    Co-authored-by: OceanLi <122793010+ohdearquant@users.noreply.github.com>

---
+++ goals/gself-pow-two-pow-22-add-pow-seven.lean
@@ -0,0 +1,4 @@
+import Mathlib
+
+theorem gself_pow_two_pow_22_add_pow_seven (n : ℤ) : (n^2) ∣ (n^22 + n^7) := by
+  sorry
---
+++ library/Unsorry/GselfPowTwoPow22AddPowSeven.lean
@@ -0,0 +1,4 @@
+import Mathlib
+
+theorem gself_pow_two_pow_22_add_pow_seven (n : ℤ) : (n^2) ∣ (n^22 + n^7) := by
+  exact ⟨n^20 + n^5, by ring⟩
