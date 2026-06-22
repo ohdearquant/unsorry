34e0ce51b90622ab9e849af23a76aca6c9d9e7f7
Chris Barlow <cgbarlow@gmail.com>
2026-06-23 08:02:23 +1200

    prove(gself-pow-two-pow-23-add-pow-fifteen): gself_pow_two_pow_23_add_pow_fifteen by mac-158f (#4828)
    Co-authored-by: OceanLi <122793010+ohdearquant@users.noreply.github.com>

---
+++ goals/gself-pow-two-pow-23-add-pow-fifteen.lean
@@ -0,0 +1,4 @@
+import Mathlib
+
+theorem gself_pow_two_pow_23_add_pow_fifteen (n : ℤ) : (n^2) ∣ (n^23 + n^15) := by
+  sorry
---
+++ library/Unsorry/GselfPowTwoPow23AddPowFifteen.lean
@@ -0,0 +1,4 @@
+import Mathlib
+
+theorem gself_pow_two_pow_23_add_pow_fifteen (n : ℤ) : (n^2) ∣ (n^23 + n^15) := by
+  exact ⟨n^21 + n^13, by ring⟩
